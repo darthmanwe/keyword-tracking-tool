0796225a5214857bf1161d2876685e8b85b7e224
snovelli <salvatore.novelli@gmail.com>
2016-05-25 23:01:00 +0100

    Preparing the controller

---
+++ src/main/java/com/snovelli/seo/ktt/controller/KeywordTrackingController.java
@@ -0,0 +1,35 @@
+package com.snovelli.seo.ktt.controller;
+
+import com.google.api.services.webmasters.model.SearchAnalyticsQueryResponse;
+import com.snovelli.seo.ktt.SearchAnalyticsClient;
+import com.snovelli.seo.ktt.model.DeviceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Locale;
+
+/**
+ * Created by Salvatore on 25/05/2016.
+ */
+@RestController
+public class KeywordTrackingController {
+
+    @Autowired
+    SearchAnalyticsClient searchAnalyticsClient;
+
+    @RequestMapping("/keyword-position")
+    public SearchAnalyticsQueryResponse getKeywordPosition(@RequestParam String query) throws IOException {
+        return searchAnalyticsClient.performRequest(
+                Period.between(LocalDate.of(2016, 4, 1), LocalDate.of(2016, 5, 1)),
+                query,
+                Locale.ITALY,
+                DeviceType.DESKTOP);
+    }
+
+
+}
---
+++ src/test/java/com/snovelli/seo/ktt/controller/KeywordTrackingControllerTest.java
@@ -0,0 +1,21 @@
+package com.snovelli.seo.ktt.controller;
+
+import com.snovelli.seo.KeywordTrackingToolApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by Salvatore on 25/05/2016.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KeywordTrackingToolApplication.class)
+@WebAppConfiguration
+public class KeywordTrackingControllerTest {
+
+
+
+
+}
