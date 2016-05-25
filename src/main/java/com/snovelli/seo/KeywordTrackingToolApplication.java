95f37485e2f6b203d866609488bca9d716c80585
snovelli <salvatore.novelli@gmail.com>
2016-05-25 21:32:02 +0100

    Added friendly homepage

--- src/main/java/com/snovelli/seo/KeywordTrackingToolApplication.java
+++ src/main/java/com/snovelli/seo/KeywordTrackingToolApplication.java
@@ -2,9 +2,19 @@ package com.snovelli.seo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class KeywordTrackingToolApplication {
+
+
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Go away!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeywordTrackingToolApplication.class, args);
