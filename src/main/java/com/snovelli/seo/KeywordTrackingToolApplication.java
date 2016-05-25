26a56532b55502a4b8c6b17ea80aa272531e5693
snovelli <salvatore.novelli@gmail.com>
2016-05-25 21:29:41 +0100

    INIT

---
+++ src/main/java/com/snovelli/seo/KeywordTrackingToolApplication.java
@@ -0,0 +1,12 @@
+package com.snovelli.seo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordTrackingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordTrackingToolApplication.class, args);
+	}
+}
