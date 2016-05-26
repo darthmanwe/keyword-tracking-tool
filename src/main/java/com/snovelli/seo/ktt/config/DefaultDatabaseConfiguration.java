0b2bb4e13b84e4c9e20d02a9d0a0abea21d68d1e
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-05-26 16:09:32 +0100

    Adding database configuration for both dev and heroku profile

---
+++ src/main/java/com/snovelli/seo/ktt/config/DefaultDatabaseConfiguration.java
@@ -0,0 +1,26 @@
+package com.snovelli.seo.ktt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
+public class DefaultDatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+
+        // no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
+                //.addScript("db/sql/create-db.sql")
+                //.addScript("db/sql/insert-data.sql")
+                .build();
+        return db;
+    }
+}
