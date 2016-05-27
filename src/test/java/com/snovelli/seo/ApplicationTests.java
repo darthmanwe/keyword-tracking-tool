86d0230d092054058fe0a0cc28929ca14f47b173
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-05-27 15:56:25 +0100

    Minor refactoring

--- src/test/java/com/snovelli/seo/ApplicationTests.java
+++ src/test/java/com/snovelli/seo/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-public class KeywordTrackingToolApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
